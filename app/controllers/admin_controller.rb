d41a9a0f919f3fde9153ec7e68654b7e317e2b35
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-03-19 23:45:35 -0300

    done update admin and site settings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,60 @@
 class AdminController < ApplicationController
-  def settings
+  def site_settings
+    if admin_logged_in?
+      if admin_logged_in?
+        @admin = current_admin
+      end
+    end
   end
 
-  def change_password
+  def settings
+    if admin_logged_in?
+      @admin = current_admin
+    end
+  end
+
+  def update_admin
+    if admin_logged_in?
+      @admin = current_admin
+      if @admin.update_attributes(admin_params)
+        flash.now[:success] = 'Settings updated.'
+        render 'settings'
+      else
+        flash.now[:danger] = @admin.errors.full_messages.join("<br>").html_safe
+        render 'settings'
+      end
+    end
+  end
+
+  def update_site
+    if admin_logged_in?
+      @admin = current_admin
+      if @admin.authenticate(site_params[:password])
+        if @admin.update_attributes(site_params)
+          flash.now[:success] = 'Site settings updated.'
+          render 'site_settings'
+        else
+          flash.now[:danger] = @admin.errors.full_messages.join("<br>").html_safe
+          render 'site_settings'
+        end
+      else
+        flash.now[:danger] = "Wrong password!"
+        render 'site_settings'
+      end
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:login,
+                                  :password,
+                                  :password_confirmation)
+  end
+
+  def site_params
+    params.require(:admin).permit(:site_title,
+                                  :site_description,
+                                  :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/contributors', to: 'pages#contributors'
 
+  get 'admin/site_settings'
+  patch 'admin/site_settings', to: 'admin#update_site'
   get 'admin/settings'
-  get 'admin/change_password'
+  patch 'admin/settings', to: 'admin#update_admin'
 
   get '/admin', to: 'admin_session#new'
   post '/admin', to: 'admin_session#create'
-  delete '/admin', to: 'admin_session#destroy'
+  delete '/admin', to: 'admin_session#destroy' 
 
   get 'contributor/settings'
   get 'contributor/change_password'
