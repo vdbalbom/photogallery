1dfe1d24a8b963016ee607a681fe4242d473a204
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-03-20 00:20:16 -0300

    done contributors settings and change password

--- app/controllers/contributor_controller.rb
+++ app/controllers/contributor_controller.rb
@@ -1,7 +1,65 @@
 class ContributorController < ApplicationController
   def settings
+    if contributor_logged_in?
+      @contributor = current_contributor
+    end
+  end
+
+  def update_settings
+    if contributor_logged_in?
+      @contributor = current_contributor
+      if @contributor.authenticate(contributor_params[:password])
+        if @contributor.update_attributes(contributor_params)
+          flash.now[:success] = 'Settings updated.'
+          render 'settings'
+        else
+          flash.now[:danger] = @contributor.errors.full_messages.join("<br>").html_safe
+          render 'settings'
+        end
+      else
+        flash.now[:danger] = "Wrong password!"
+        render 'settings'
+      end
+    end
   end
 
   def change_password
+    if contributor_logged_in?
+      @contributor = current_contributor
+    end
   end
+
+  def update_password
+    if contributor_logged_in?
+      @contributor = current_contributor
+      if @contributor.update_attributes(change_password_params)
+        flash.now[:success] = 'Password updated.'
+        render 'change_password'
+      else
+        flash.now[:danger] = @contributor.errors.full_messages.join("<br>").html_safe
+        render 'change_password'
+      end
+    end
+  end
+
+  private
+
+  def contributor_params
+    params.require(:contributor).permit(:name,
+                                        :profile_picture,
+                                        :styles,
+                                        :description,
+                                        :email,
+                                        :facebook_link,
+                                        :instagram_link,
+                                        :tumblr_link,
+                                        :login,
+                                        :password)
+  end
+
+  def change_password_params
+    params.require(:contributor).permit(:password,
+                                        :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,12 @@ Rails.application.routes.draw do
 
   get '/admin', to: 'admin_session#new'
   post '/admin', to: 'admin_session#create'
-  delete '/admin', to: 'admin_session#destroy' 
+  delete '/admin', to: 'admin_session#destroy'
 
   get 'contributor/settings'
+  patch 'contributor/settings', to: 'contributor#update_settings'
   get 'contributor/change_password'
+  patch 'contributor/change_password', to: 'contributor#update_password'
 
   get '/login', to: 'contributor_session#new'
   post '/login', to: 'contributor_session#create'
