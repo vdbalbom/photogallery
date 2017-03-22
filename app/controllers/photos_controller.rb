83027f381b37559259d7f717bcf20c6c7e032d25
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-03-22 17:24:09 -0300

    fix the format_tag_list_temp method and implement validation of tag_list_temp

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,13 +22,17 @@ class PhotosController < ApplicationController
     if contributor_logged_in?
       @photo = Photo.new(photo_params)
       @photo.contributor = current_contributor
-      if @photo.save
-        @photo.tag_list_temp = format_tag_list_temp @photo.tag_list_temp
-        flash.now[:success] = 'New photo uploaded.'
-        render 'new'
+      if tag_list_temp_is_valid? @photo.tag_list_temp
+        if @photo.save
+          @photo.tag_list_temp = format_tag_list_temp @photo.tag_list_temp
+          flash.now[:success] = 'New photo uploaded.'
+          render 'new'
+        else
+          flash.now[:danger] = @photo.errors.full_messages.join("<br>").html_safe
+          render 'new'
+        end
       else
-        flash.now[:danger] = @photo.errors.full_messages.join("<br>").html_safe
-        render 'new'
+        flash.now[:danger] = "Tag list is invalid."
       end
     end
   end
@@ -62,17 +66,26 @@ class PhotosController < ApplicationController
 
   private
 
+    def tag_list_temp_is_valid? tag_list_temp
+      # removing matches the remainder needs to be empty
+      tag_list_temp.gsub(/(#(\w|\s|&)*(\w|&)+(\w|\s|&)*)*/, "").empty?
+    end
+
     def format_tag_list_temp tag_list_temp
-      # squeeze(' ') removes extra spaces
-      # rstrip removes space in final of the string
-      # gsub(' #','#').gsub('# ','#') replaces all ' #' or '# ' for '#' in the string
-      # [1..-1] discards the # in the beggining of the string
-      # split('#') makes a tag array
-      # uniq removes duplicated tags of the tag array
-      # join(' #') transforms the array into a string separated by ' #'
-      # '#' + in the beggining of the sentence puts the '#'
-      #    in the beggining of the string again
-      '#' + tag_list_temp.squeeze(' ').rstrip.gsub(' #', '#').gsub('# ','#')[1..-1].split('#').uniq.join(' #')
+      if tag_list_temp.remove(' ').empty?
+        ''
+      else
+        # squeeze(' ') removes extra spaces
+        # rstrip removes space in final of the string
+        # gsub(' #','#').gsub('# ','#') replaces all ' #' or '# ' for '#' in the string
+        # [1..-1] discards the # in the beggining of the string
+        # split('#') makes a tag array
+        # uniq removes duplicated tags of the tag array
+        # join(' #') transforms the array into a string separated by ' #'
+        # '#' + in the beggining of the sentence puts the '#'
+        #    in the beggining of the string again
+        '#' + tag_list_temp.squeeze(' ').rstrip.gsub(' #', '#').gsub('# ','#')[1..-1].split('#').uniq.join(' #')
+      end
     end
 
     def photo_params
