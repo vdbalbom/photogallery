418e9a2daa02859f94800189dc7b1ce70c8e56a7
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-03-22 22:32:42 -0300

    links to tags and contributors photo filters, name of filtred galleries

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -4,14 +4,18 @@ class GalleryController < ApplicationController
   end
 
   def contributor_photos
+    @contributor = Contributor.find(params[:contributor_id])
     @photos = Contributor.find(params[:contributor_id]).photos
   end
 
   def tag_photos
+    @tag = Tag.find(params[:tag_id])
     @photos = Tag.find(params[:tag_id]).photos
   end
 
   def contributor_tag_photos
+    @tag = Tag.find(params[:tag_id])
+    @contributor = Contributor.find(params[:contributor_id])
     @photos = Contributor.find(params[:contributor_id]).photos & Tag.find(params[:tag_id]).photos
   end
 end
