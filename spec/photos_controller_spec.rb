0e268a34b6176f565cbe7b74809af2ae6fdf7971
Virginia Monnerat <virginiamonnerat@gmail.com>
2017-04-28 14:20:51 -0300

    fix and refatorate photos_controller_spec to work with authentication_helper

--- spec/photos_controller_spec.rb
+++ spec/photos_controller_spec.rb
@@ -1,32 +1,10 @@
-require 'rack/test'
-
 describe PhotosController, type: :controller do
 
-  before do
-    @contributor = Contributor.create(name: "test456", login: "test456", password: "test456", email: "test456@sample.com")
-    @admin = Admin.create(login: "admin456", password: "admin456", site_title: "test site name", site_description: "test description")
-  end
-
-  def test_with_contributor_logged_in_and_admin_logged_out
-    allow(controller).to receive(:current_contributor).and_return(@contributor)
-    allow(controller).to receive(:current_admin).and_return(nil)
-  end
-
-  def test_with_contributor_and_admin_logged_out
-    allow(controller).to receive(:current_contributor).and_return(nil)
-    allow(controller).to receive(:current_admin).and_return(nil)
-  end
-
-  def test_with_contributor_logged_out_and_admin_logged_in
-    allow(controller).to receive(:current_contributor).and_return(nil)
-    allow(controller).to receive(:current_admin).and_return(@admin)
-  end
-
   it "create_photo" do
-    test_with_contributor_logged_in_and_admin_logged_out
+    contributor_sign_in "testlogin", "testpassword"
 
     post :create, params: {photo: {title: 'photo test', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#test1 #test2 #test3'}}
-    expect(Photo.last.contributor).to eq(@contributor)
+    expect(Photo.last.contributor.login).to eq("testlogin")
     expect(Photo.last.title).to eq('photo test')
     expect(Photo.last.description).to eq('')
     expect(Photo.last.tag_list_temp).to eq('#test1 #test2 #test3')
@@ -35,24 +13,23 @@ describe PhotosController, type: :controller do
     expect(Tag.find_by(name: "test2").nil?).to eq(false)
     expect(Tag.find_by(name: "test3").nil?).to eq(false)
 
-    test_with_contributor_and_admin_logged_out
+    admin_sign_in "testlogin", "testpassword"
 
     post :create, params: {photo: {title: 'photo fail', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: ''}}
     expect(Photo.find_by(title: 'photo fail')).to be(nil)
 
-    test_with_contributor_logged_out_and_admin_logged_in
+    sign_out
 
     post :create, params: {photo: {title: 'photo fail', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: ''}}
     expect(Photo.find_by(title: 'photo fail')).to be(nil)
-
   end
 
   it "edit_photo" do
-    test_with_contributor_logged_in_and_admin_logged_out
+    contributor_sign_in "testlogin", "testpassword"
 
     post :create, params: {photo: {title: 'photo test', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#test4 #test5 #test6'}}
     patch :update, params: {photo_id: Photo.last.id, photo: {title: 'photo test 2', description: 'edited', tag_list_temp: '#test4 #test7'}}
-    expect(Photo.last.contributor).to eq(@contributor)
+    expect(Photo.last.contributor.login).to eq("testlogin")
     expect(Photo.last.title).to eq('photo test 2')
     expect(Photo.last.description).to eq('edited')
     expect(Photo.last.tag_list_temp).to eq('#test4 #test7')
@@ -62,19 +39,19 @@ describe PhotosController, type: :controller do
     expect(Tag.find_by(name: "test6").nil?).to eq(true)
     expect(Tag.find_by(name: "test7").nil?).to eq(false)
 
-    test_with_contributor_and_admin_logged_out
+    admin_sign_in "testlogin", "testpassword"
 
     patch :update, params: {photo_id: Photo.last, photo: {title: 'photo fail', description: '', tag_list_temp: ''}}
     expect(Photo.find_by(title: 'photo fail')).to be(nil)
 
-    test_with_contributor_logged_out_and_admin_logged_in
+    sign_out
 
     patch :update, params: {photo_id: Photo.last, photo: {title: 'photo fail', description: '', tag_list_temp: ''}}
     expect(Photo.find_by(title: 'photo fail')).to be(nil)
   end
 
   it "delete_photo" do
-    test_with_contributor_logged_in_and_admin_logged_out
+    contributor_sign_in "testlogin", "testpassword"
 
     post :create, params: {photo: {title: 'photo to be deleted', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#testtobedeleted'}}
     delete :delete, params: {photo_id: Photo.last.id}
@@ -84,28 +61,31 @@ describe PhotosController, type: :controller do
     post :create, params: {photo: {title: 'photo to be deleted', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#testtobedeleted'}}
     photo_last_id = Photo.last.id
 
-    test_with_contributor_and_admin_logged_out
+    sign_out
 
     delete :delete, params: {photo_id: photo_last_id}
     expect(Photo.find_by(title: 'photo to be deleted')).not_to eq(nil)
 
-    test_with_contributor_logged_out_and_admin_logged_in
+    admin_sign_in "testlogin", "testpassword"
 
     delete :delete, params: {photo_id: photo_last_id}
     expect(Photo.find_by(title: 'photo to be deleted')).to eq(nil)
 
+    sign_out
   end
 
   it "uniq tags" do
-    test_with_contributor_logged_in_and_admin_logged_out
+    contributor_sign_in "testlogin", "testpassword"
 
     post :create, params: {photo: {title: 'photo test', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#test8 #test8 #test8'}}
     expect(Photo.last.tag_list_temp).to eq('#test8')
     expect(Photo.last.tags.count).to eq(1)
+
+    sign_out
   end
 
   it "do not delete tags being used" do
-    test_with_contributor_logged_in_and_admin_logged_out
+    contributor_sign_in "testlogin", "testpassword"
 
     post :create, params: {photo: {title: 'photo test', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#test9 #test10 #test11'}}
     post :create, params: {photo: {title: 'photo test', description: '', image: Rack::Test::UploadedFile.new('spec/fixtures/images/rails.png', 'image/png'), tag_list_temp: '#test10 #test11 #test12'}}
@@ -119,6 +99,7 @@ describe PhotosController, type: :controller do
     expect(Tag.find_by(name: "test11").nil?).to eq(false)
     expect(Tag.find_by(name: "test12").nil?).to eq(true)
 
+    sign_out
   end
 
 end
